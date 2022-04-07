e4daa530085efbbff3bc9e8d8cf2e9d4e6abed98
fulviomariola <fulvio.mariola@gmail.com>
2022-04-07 16:14:51 +0200

    Esercizi del corso

---
+++ 07_LeVariabili/main.py
@@ -0,0 +1,7 @@
+calcolo_ore = 24
+nome_ore = "ore"
+
+print(f"in 20 giorni ho {20 * calcolo_ore} {nome_ore}")
+print(f"in 40 giorni ho {40 * calcolo_ore} {nome_ore}")
+print(f"in 65 giorni ho {65 * calcolo_ore} {nome_ore}")
+print(f"in 130 giorni ho {130 * calcolo_ore} {nome_ore}")
